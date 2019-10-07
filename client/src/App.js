54b15b04b062315075ebfa1fae18830020b995c0
Marcin Kaleta <kalet1984@gmail.com>
2019-10-07 19:30:00 +0200

    Finish testing server-client connection

--- client/src/App.js
+++ client/src/App.js
@@ -5,11 +5,28 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        Hello world!
+        <ul>
+            {this.state.posts.map(post => <li key={post.id}>{post.title}</li>)}
+        </ul>
       </div>
     );
   }
+  
+  //to test server-client connection
+  state = {
+    posts: [],
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:8000/api/posts')
+      .then(res => res.json())
+      .then(res => {
+        this.setState({ posts: res });
+    });
+  }
 
 };
+
+
 
 export default App;
