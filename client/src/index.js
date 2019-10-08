e79dc7c552fcff26ebb3a951c9f3bbb926e81330
Marcin Kaleta <kalet1984@gmail.com>
2019-10-08 16:34:24 +0200

    Add global styles and style packages

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './styles/global.scss';
+import 'animate.css/animate.min.css';
 import App from './App';
 
 const Root = () => (
