5117829a025e320515f2ce01793a5bbe4c1af686
Marcin Kaleta <kalet1984@gmail.com>
2019-10-12 13:14:46 +0200

    Add dev-tools

--- client/src/redux/postsRedux.js
+++ client/src/redux/postsRedux.js
@@ -1,3 +1,6 @@
+import axios from 'axios'; // much simpler sendin req to server 
+
+
 /* SELECTORS */
 
 export const getPosts = ({ posts }) => posts;
@@ -19,16 +22,16 @@ const initialState = [];
 /* THUNKS */
 
 export const loadPostsRequest = () => {
-    return dispatch => {
-  
-      console.log('Request started...');
-      setTimeout(() => {
-        const arr = [{ id: 'a3fssdc1', title: 'Test', content: 'Lorem Ipsum' }];
-        dispatch(loadPosts(arr));
-        console.log('Request finished after 2sec!');
-      }, 2000);
-  
-    };
+  return dispatch => {
+
+    axios.get('http://localhost:8000/api/posts').then(res => {
+      dispatch(loadPosts(res.data));
+    })
+    .catch(err => {
+      console.log(err.message);
+    });
+
+  };
 };
 
 /* REDUCER */
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,4 +1,4 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 
 // import reducers
@@ -9,6 +9,12 @@ const rootReducer = combineReducers({
   posts,
 });
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(
+  rootReducer,
+  compose(
+		applyMiddleware(thunk),
+		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+	)
+);
 
 export default store;
