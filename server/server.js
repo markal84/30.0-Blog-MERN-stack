1e5013d44a48bf0f96b891bf52b05517e40c65ff
Marcin Kaleta <kalet1984@gmail.com>
2019-10-14 20:51:17 +0200

    Finish server improvments

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const config = require('./config');
 const mongoose = require('mongoose');
+const helmet = require('helmet'); //basic security
+const loadTestData = require('./testData');
 // import routes
 const postRoutes = require('./routes/post.routes');
 
@@ -11,6 +13,7 @@ const app = express();
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(helmet());
 //routes
 app.use('/api', postRoutes);
 
@@ -18,7 +21,10 @@ app.use('/api', postRoutes);
 mongoose.connect(config.DB, { useNewUrlParser: true });
 let db = mongoose.connection;
 
-db.once('open', () => console.log('Connected to the database'));
+db.once('open', () => {
+  console.log('Connected to the database');
+  loadTestData();
+});
 db.on('error', (err) => console.log('Error ' + err));
 
 //server start
