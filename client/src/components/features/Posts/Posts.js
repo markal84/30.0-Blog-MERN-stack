245e07dd536913f5b39df5d8cdf574ea95ca1231
Marcin Kaleta <kalet1984@gmail.com>
2019-10-14 19:32:50 +0200

    Finish spinner requests implementation

--- client/src/components/features/Posts/Posts.js
+++ client/src/components/features/Posts/Posts.js
@@ -3,6 +3,7 @@ import { PropTypes } from 'prop-types';
 
 import Spinner from '../../common/Spinner/Spinner';
 import PostsList from '../PostsList/PostsList';
+import Alert from '../../common/Alert/Alert';
 
 class Posts extends React.Component {
 
@@ -13,13 +14,38 @@ class Posts extends React.Component {
 
   render() {
     const { posts, request } = this.props;
-
-    return (
-      <div>
-        {request.pending && <Spinner />}
-        <PostsList posts={posts} />
-      </div>
-    );
+  
+    if (request.pending === false && request.success === true && posts.length > 0) {
+      return (
+          <div>
+              <PostsList posts={posts}/>
+          </div>
+      );
+    } else if (request.pending === true || request.success === null) {
+      return (
+          <div>
+              <Spinner/>
+          </div>
+      );
+    } else if (request.pending === false && request.error !== null) {
+      return (
+          <div>
+              <Alert variant={'error'}>{request.error}</Alert>
+          </div>
+      );
+    } else if (request.pending === false && request.success === true && posts.length === 0) {
+      return (
+          <div>
+              <Alert variant={'info'}>No posts</Alert>
+          </div>
+      );
+    } else {
+      return (
+          <div>
+              <Alert variant={'info'}>Error...</Alert>
+          </div>
+      );
+    }
   }
 
 };
