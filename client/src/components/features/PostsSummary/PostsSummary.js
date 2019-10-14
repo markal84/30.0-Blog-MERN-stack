2638f256eb4527042761001a9f75c7d11e1eceb0
Marcin Kaleta <kalet1984@gmail.com>
2019-10-14 19:47:43 +0200

    Add cutText function

--- client/src/components/features/PostsSummary/PostsSummary.js
+++ client/src/components/features/PostsSummary/PostsSummary.js
@@ -5,11 +5,12 @@ import '../PostsSummary/PostsSummary.scss';
 import HtmlBox from '../../common/HtmlBox/HtmlBox';
 import Button from '../../common/Button/Button';
 import SmallTitle from '../../common/SmallTitle/SmallTitle';
+import cutText from '../../common/cutText/cutText';
 
 const PostSummary = ({ id, title, content }) => (
     <article className="post-summary">
       <SmallTitle>{title}</SmallTitle>
-      <HtmlBox>{content}</HtmlBox>
+      <HtmlBox>{cutText(content, 250)}</HtmlBox>
       <Button variant="primary">
         Read more
       </Button>
