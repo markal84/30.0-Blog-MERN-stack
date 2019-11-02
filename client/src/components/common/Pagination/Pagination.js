0055a18a69e65430254c969f253bb2115ec45264
Marcin Kaleta <kalet1984@gmail.com>
2019-11-02 14:07:20 +0100

    Try to fix pagination icons

--- client/src/components/common/Pagination/Pagination.js
+++ client/src/components/common/Pagination/Pagination.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+
 import './Pagination.scss';
 
 class Pagination extends React.Component {
@@ -16,28 +17,56 @@ class Pagination extends React.Component {
     onPageChange(newPage);
   }
 
+	prevPage = () => {
+    const { presentPage } = this.state;
+    const { changePage } = this;
+    if (presentPage > 1 )
+    changePage(presentPage - 1)
+  };
+
+  nextPage = () => {
+  	const { pages } = this.props;
+    const { presentPage } = this.state;
+    const { changePage } = this;
+    if (presentPage < pages )
+    changePage(presentPage + 1)
+  };
+  
+
   render() {
 
     const { pages } = this.props;
     const { presentPage } = this.state;
-    const { changePage } = this;
+    const { changePage, prevPage, nextPage } = this;
   
     return (
-      <div className="pagination">
-        <ul className="pagination__list">
-  
-          {[...Array(pages)].map((el, page) =>
-          <li
-              key={++page}
-              onClick={() => { changePage(page) }}
-              className={`pagination__list__item${((page) === presentPage) ? ' pagination__list__item--active' : ''}`}>
-              {page}
-          </li>
-          )}
-  
-        </ul>
-      </div>
-    );
+	    <div className="pagination">
+	      <ul className="pagination__list">
+	      	{ presentPage >= 2 ? ( //ES6 version of if
+            <li
+              className={`pagination__list__item${(true) ? ' pagination__list__item--active' : '' }`}
+	            onClick={prevPage}>
+	            &lt;
+            </li>) : ''}
+
+	        {[...Array(pages)].map((el, page) => (
+		        <li
+		            key={++page}
+		            onClick={() => { changePage(page) }}
+		            className={`pagination__list__item${((page) === presentPage) ? ' pagination__list__item--active' : ''}`}>
+		            {page}
+		        </li>
+	        ))}
+
+	        {presentPage === pages ?  '' : (
+            <li
+              className={`pagination__list__item${(true) ? ' pagination__list__item--active' : '' }`}
+              onClick={nextPage}>
+              &gt;
+          	</li>)}
+	      </ul>
+	    </div>
+	  );
   }
 }
 
