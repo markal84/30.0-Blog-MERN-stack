528d420c29816bff352720181a8ab52b64c4fc49
Marcin Kaleta <kalet1984@gmail.com>
2019-11-04 16:50:26 +0100

    Pushing build version

---
+++ client/build/precache-manifest.e87b9164e5bf7a6c2ee8fdd0e5bc0b1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a55a8a390b614230d5c8e9446b57ff2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3a0209e9a8356703335",
+    "url": "/static/css/2.b569b48b.chunk.css"
+  },
+  {
+    "revision": "4f9e844b89178f347cd5",
+    "url": "/static/css/main.3a1304ba.chunk.css"
+  },
+  {
+    "revision": "d3a0209e9a8356703335",
+    "url": "/static/js/2.b71b6afb.chunk.js"
+  },
+  {
+    "revision": "4f9e844b89178f347cd5",
+    "url": "/static/js/main.4ad5d95e.chunk.js"
+  },
+  {
+    "revision": "875d570ce1b6fc43afc6",
+    "url": "/static/js/runtime-main.e077b804.js"
+  }
+]);
